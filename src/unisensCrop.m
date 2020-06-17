83341ca4ac54b8127448f9c61e13225d523384f4
Vincent Abt <abt@corvolution.com>
2020-06-17 16:31:31 +0200

    minor fix

--- src/unisensCrop.m
+++ src/unisensCrop.m
@@ -145,7 +145,7 @@ function signal_entry_crop(j_entry, j_unisens_cropped, samplestamp_start, sample
         if ~isempty(data)
             % append is terrible slow, try to use fwrite instead
             % j_entry_cropped.append(data);
-            fwrite(h, data, lower(char(datatype)));
+            fwrite(h, data, datatype);
         end
         position = position + count;
     end
