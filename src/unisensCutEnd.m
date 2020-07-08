8d53f04e1b96020352201c404f51f4a7ac3ac589
Vincent Abt <abt@ad.corvolution.com>
2020-07-08 14:49:10 +0200

    keep all data in the original dataset folder by only overwriting the cropped unisens dataset

--- src/unisensCutEnd.m
+++ src/unisensCutEnd.m
@@ -1,16 +1,46 @@
-function unisensCutEnd(path, duration, new_path)
+function [status, message] = unisensCutEnd(path, duration, new_path)
 %UNISENSCUTEND cut off the end of a unisens dataset
 
 % Copyright 2017 movisens GmbH, Germany
+message = '';
+status = 1;
 
-    if nargin ==2
-        temp_path = tempname();
-        unisensCrop(path, temp_path, 1, 0, duration);
-        rmdir(path,'s');
-        movefile(temp_path, path);
+if nargin == 2
+    % generate a random uinique temporary path
+    temp_path = tempname();
+    
+    % crop the dataset
+    unisensCrop(path, temp_path, 1, 0, duration);
+    
+    % list all files in the original directory
+    tempContent = dir(temp_path);
+    
+    status = ones(1, length(tempContent));
+    for i = 1 : length(tempContent)
+        % move each file to the original directory and check for possible
+        % errors while moving (this way even non unisens files from the
+        % original dataset, logs etc, will be copied in the original 
+        % directory)
+        if isfile(fullfile(tempContent(i).folder, tempContent(i).name))
+            [status(i), tmpMessage] = movefile( ...
+                fullfile(tempContent(i).folder, tempContent(i).name), ...
+                path, 'f');
+            if ~isempty(tmpMessage)
+                message = [tmpMessage newline];
+            end
+        end
     end
     
-    if nargin == 3
-        unisensCrop(path, new_path, 1, 0, duration);
+    % delete the temp directory to free up space
+    rmdir(temp_path,'s');
+    
+    % if an error occurred, set th status flag accordingly
+    if sum(status) > length(tempContent)
+        status = 0;
+    else
+        status = 1;
     end
+elseif nargin == 3
+    unisensCrop(path, new_path, 1, 0, duration);
+end
 end
